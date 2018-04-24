268fcd0c8ad118803710f5347fef68be97054c25
Lamia Haider <lamia.haider@gmail.com>
2018-04-24 05:01:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
   name_hash.each do |key, value|
-    binding.binding.pry
+    binding.pry
     if min_value == nil
       min_key = key
       min_value = value
